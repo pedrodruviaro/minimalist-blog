3475188fb3fddafd51ce70d6c7e6093a646254dc
pedrodruviaro <pedrodruviaro@gmail.com>
2024-06-03 16:07:58 -0300

    feat: :lipstick: add google fonts and refactor scss filenames

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,17 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
+  modules: ["@nuxtjs/google-fonts"],
+  googleFonts: {
+    download: true,
+    families: {
+      "Hanken Grotesk": [400, 700],
+    },
+  },
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "~/assets/scss/_colors.scss" as *;',
+          additionalData: '@use "~/assets/scss/_variables.scss" as *;',
         },
       },
     },
